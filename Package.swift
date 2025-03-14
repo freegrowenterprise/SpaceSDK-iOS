55ba0ac8ee02369885c60f466a3af4ff3077b861
Mingwan Choi <wbsldj7645@naver.com>
2025-03-14 10:38:28 +0900

    delete Exec

--- Package.swift
+++ Package.swift
@@ -13,20 +13,12 @@ let package = Package(
         .library(
             name: "GrowSpaceSDK",
             targets: ["GrowSpaceSDK"]),
-        .executable(
-            name: "GrowSpaceSDKExec",
-            targets: ["GrowSpaceSDKExec"]
-        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GrowSpaceSDK"),
-        .executableTarget(
-            name: "GrowSpaceSDKExec",
-            dependencies: ["GrowSpaceSDK"]
-        ),
         .testTarget(
             name: "GrowSpaceSDKTests",
             dependencies: ["GrowSpaceSDK"]
--- Sources/GrowSpaceSDKExec/main.swift
+++
@@ -1,8 +0,0 @@
-import GrowSpaceSDK
-import Foundation
-
-let sdk = GrowSpaceSDK()
-sdk.startSearchGrowSpaceBeacon()
-print("실행 완료")
-
-RunLoop.main.run()
