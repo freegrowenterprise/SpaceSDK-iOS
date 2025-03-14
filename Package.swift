e53dcea0f0669f71e75aa036aadbca40ac6e5ae6
Mingwan Choi <wbsldj7645@naver.com>
2025-03-14 10:49:27 +0900

    add dependencies

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
             name: "GrowSpaceSDK",
             targets: ["GrowSpaceSDK"]),
     ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
