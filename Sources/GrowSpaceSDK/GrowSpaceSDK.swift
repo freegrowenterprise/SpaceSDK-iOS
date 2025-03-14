8da8f2260c74e820fee12088a2b9a4b19b2b56e7
Mingwan Choi <wbsldj7645@naver.com>
2025-03-14 11:06:58 +0900

    edit target version

--- Sources/GrowSpaceSDK/GrowSpaceSDK.swift
+++ Sources/GrowSpaceSDK/GrowSpaceSDK.swift
@@ -1,5 +1,11 @@
-import CoreBluetooth
+//
+//  GrowSpaceSDK.swift
+//  GrowSpaceSDK
+//
+//  Created by min gwan choi on 3/14/25.
+//
 
+import CoreBluetooth
 
 public class GrowSpaceSDK {
     private var scanner: BLEScanner?
