8016938c81670bccf60437a3dd85e4a926fc1432
Mingwan Choi <wbsldj7645@naver.com>
2025-04-24 10:06:40 +0900

    Add Logger

--- Sources/GrowSpaceSDK/GrowSpaceSDK.swift
+++ Sources/GrowSpaceSDK/GrowSpaceSDK.swift
@@ -13,13 +13,14 @@ public class GrowSpaceSDK {
     private let apiKey: String
     
     private var scanner: SpaceBeaconScanner?
+    private var uwbScanner: SpaceUWB = SpaceUWB()
     
     public init(apiKey: String) {
         self.apiKey = apiKey
     }
     
     public func startSearchGrowSpaceBeacon(onDiscoverDevices: @escaping (Result<SpaceZoneResponse?, Error>) -> Void) {
-        print("startSearchGrowSpaceBeacon 실행")
+        DebugLogger.debug("startSearchGrowSpaceBeacon 실행")
         if scanner == nil {
             scanner = SpaceBeaconScanner(
                 apiKey: self.apiKey
@@ -31,9 +32,28 @@ public class GrowSpaceSDK {
     
     public func stopSearchGrowSpaceBeacon() {
         if let scanner = scanner {
-            print("stopSearchGrowSpaceBeacon 실행")
+            DebugLogger.debug("stopSearchGrowSpaceBeacon 실행")
             scanner.stopScanning()
             self.scanner = nil
+        }
+    }
+    
+    public func startUWBRanging() {
+        uwbScanner.start()
+        
+        uwbScanner.spaceUWBHandler = {
+            DebugLogger.debug("device Name : \($0.deviceName) distance : \($0.distance) azimuth : \($0.azimuth) elevation : \($0.elevation)")
+        }
+        
+        uwbScanner.spcaeUWBDisconnectHandler = {
+            switch $0.disConnectType {
+            case .deviceRemoved:
+                DebugLogger.debug("deviceRemoved")
+            case .disconnectedDueToDistance:
+                DebugLogger.debug("disconnectedDueToDistance")
+            default:
+                break
+            }
         }
     }
 }
