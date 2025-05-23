67dd007063d26b94247a127d4b5cfc019fba3de7
Mingwan Choi <wbsldj7645@naver.com>
2025-05-23 13:42:40 +0900

    remove api key

--- Sources/GrowSpaceSDK/GrowSpaceSDK.swift
+++ Sources/GrowSpaceSDK/GrowSpaceSDK.swift
@@ -10,33 +10,7 @@ import CoreBluetooth
 import GrowSpacePrivateSDK
 
 public class GrowSpaceSDK {
-    private let apiKey: String
-    
-    private var scanner: SpaceBeaconScanner?
     private var uwbScanner: SpaceUWB = SpaceUWB()
-    
-    public init(apiKey: String) {
-        self.apiKey = apiKey
-    }
-    
-    public func startSearchGrowSpaceBeacon(onDiscoverDevices: @escaping (Result<SpaceZoneResponse?, Error>) -> Void) {
-        DebugLogger.debug("startSearchGrowSpaceBeacon 실행")
-        if scanner == nil {
-            scanner = SpaceBeaconScanner(
-                apiKey: self.apiKey
-            )
-            
-            scanner?.startScanning(onDeviceDiscovered: onDiscoverDevices)
-        }
-    }
-    
-    public func stopSearchGrowSpaceBeacon() {
-        if let scanner = scanner {
-            DebugLogger.debug("stopSearchGrowSpaceBeacon 실행")
-            scanner.stopScanning()
-            self.scanner = nil
-        }
-    }
     
     public func startUWBRanging(
         maximumConnectionCount: Int = 4,
