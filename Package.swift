35fd7a384f888d7b2b9941b814091566f0b4b92f
Mingwan Choi <wbsldj7645@naver.com>
2025-08-13 13:16:16 +0900

    chore chagne sdk version

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
 
         .binaryTarget(
             name: "GrowSpacePrivateSDK",
-            url: "https://github.com/freegrowenterprise/SpaceSDK-iOS/releases/download/v0.0.33/GrowSpacePrivateSDK.xcframework.zip",
-            checksum: "09e721fc43cac318889e368b3f5aa4e8328ac0cb3c79ce7e0770b14ba16648ec"
+            url: "https://github.com/freegrowenterprise/SpaceSDK-iOS/releases/download/v0.0.34/GrowSpacePrivateSDK.xcframework.zip",
+            checksum: "22ce5ca1e24611767bec1838686cf84f66c86987416709462ed4a0a0f2cba0a6"
         ),
         .testTarget(
             name: "GrowSpaceSDKTests",
